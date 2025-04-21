03f28c36fc3e1ddf7ffa98f00c94ae695b3dff51
Diego Asanza <f.asanza@de.abb.com>
2025-04-21 12:16:05 +0200

    f
    Signed-off-by: Diego Asanza <f.asanza@de.abb.com>

--- include/modbus/modbus_rtu.h
+++ include/modbus/modbus_rtu.h
@@ -21,14 +21,18 @@ typedef int (*mbrtu_send_async)(uint8_t* buf, int len, mbrtu_txcb cb);
 typedef int (*mbrtu_recv_async)(uint8_t* buf, int len, mbrtu_rxcb cb);
 typedef int (*mbrtu_tmr_start)(uint32_t period, mbrtu_tmrcb cb);
 
-struct mbrtu_state {
+struct mbrtu {
+    struct mb mb;
     mbrtu_send_async send;
     mbrtu_recv_async  rcv;
     mbrtu_tmr_start   tmr;
+};
+
+struct mbrtu_state {
     uint8_t buf[MBRTU_MAX_PDU_SIZE];
 };
 
 int
-mbrtu_poll(const struct mb, const struct mbrtu_state* state);
+mbrtu_poll(const struct mbrtu, const struct mbrtu_state* state);
 
 #endif /* MODBUS_RTU_H_FF363922_5961_4B3B_B7A7_4FA56794B9CD */
